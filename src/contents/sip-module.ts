534f7873dcf35317f8ffd1eaeba89319e33adfed
winpzs <winpzs@gmail.com>
2018-01-07 09:58:32 +0800

    ADD reg sip moduel

--- src/contents/sip-module.ts
+++ src/contents/sip-module.ts
@@ -7,6 +7,19 @@ export class SipModule implements ContentBase {
 
     prefix = 'module';
 
+    private isSip(fsFile:string):boolean{
+        let exi = /[\\\/]sip[\\\/]/i.exec(fsFile);
+        if (exi){
+            if (/[\\\/](?:sip\-core|sip-shared)/i.test(fsFile)) return false;
+            let find = exi[0];
+            let fsPath = fsFile.substr(0, exi.index + find.length);
+            fsPath = path.join(fsPath, 'sip-shared');
+            return fs.existsSync(fsPath);
+        } else {
+            return false;
+        }
+    }
+
     generate(params: GenerateParam): string {
         let name = params.name,
             prefix = this.prefix;
@@ -19,6 +32,8 @@ export class SipModule implements ContentBase {
 
         let retFile = '',
             fsFile;
+
+        params.isSip = this.isSip(fsPath);
 
         if (params.ts) {
             fsFile = path.join(fsPath, MakeFileName(name, prefix, 'ts'));
@@ -49,15 +64,18 @@ export class SipModule implements ContentBase {
         let name = params.name;
         let prefix = this.prefix;
         let className = MakeClassName(name, prefix);
+        let isSip = params.isSip;
+        let sipImport = isSip ? `import { SipSharedModule } from '@sip/sip-shared/sip-shared.module';\n` : '';
+        let sipModuleImport = isSip ? `,\n        SipSharedModule` : '';
 
         let content = `import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
-
+${sipImport}
 @NgModule({
     imports: [
         CommonModule,
-        SharedModule
+        SharedModule${sipModuleImport}
     ],
     declarations: [],
     providers: [],
@@ -78,17 +96,21 @@ export class ${className} { }
         let importModule = params.ts ? `import { ${ipClassName} } from './${params.name}.module';\n` : '';
         let importClass  = params.ts ? '\n        ' + ipClassName + ',' : '';
 
+        let isSip = params.isSip;
+        let sipImport = isSip ? `import { SipSharedModule } from '@sip/sip-shared/sip-shared.module';\n` : '';
+        let sipModuleImport = isSip ? `,\n        SipSharedModule` : '';
+
         let content = `import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
-${importModule}
+${importModule}${sipImport}
 const routes: Routes = [];
 
 @NgModule({
     imports: [
         CommonModule,
-        SharedModule,${importClass}
+        SharedModule${sipModuleImport},${importClass}
         RouterModule.forChild(routes)
     ]
 })
